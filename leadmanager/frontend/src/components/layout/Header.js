4d92fbd53323e029ca84d14fe59d633d0deab38b
Ahmad <ahmad@gmail.com>
2019-09-07 20:33:21 +0300

    first_commit

---
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+
+import { Link } from "react-router-dom";
+
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/auth";
+
+export class Header extends Component {
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+    const authLinks = (
+      <ul ClassName="navbar-nav mr-auto mt-2 mt-lg-0">
+        <span className="navbar-nav ml-auto mt-2 mt-lg-0">
+          <strong>{user ? `welcome ${user.username}` : ""}</strong>
+        </span>
+        <li className="nav-item">
+          <button
+            onClick={this.props.logout}
+            className="nav-link btn btn-info btn-sm text-light"
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+    );
+
+    const guestLinks = (
+      <ul ClassName="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
+    return (
+      <nav ClassName="navbar navbar-expand-sm navbar-light bg-light">
+        <div className="container">
+          <button
+            ClassName="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span ClassName="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a ClassName="navbar-brand" href="#">
+              Lead MAnagers
+            </a>
+          </div>
+          {isAuthenticated ? authLinks : guestLinks}
+        </div>
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Header);
---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,61 @@
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+// regarding the ../ first we go to leads folders which is containing this file, then ../ then we go to components which the same, from there we go to actions and so on
+import { getLeads, deleteLead } from "../../actions/leads";
+
+export class Leads extends Component {
+  static propTypes = {
+    leads: PropTypes.array.isRequired,
+    getLeads: PropTypes.func.isRequired,
+    deleteLead: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
+  }
+  render() {
+    return (
+      <Fragment>
+        <h2>Leads</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Names</th>
+              <th>Emailss</th>
+              <th>Messagess</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map(lead => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button
+                    onClick={this.props.deleteLead.bind(this, lead.id)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(
+  mapStateToProps,
+  { getLeads, deleteLead }
+)(Leads);
